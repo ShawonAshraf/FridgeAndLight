a342d1fd970a98b3cb62efbb8dd9db799b5d9a43
Shawon Ashraf <shawon13@live.com>
2017-04-26 23:43:23 +0600

    Updated access token

--- source/access_token.py
+++ source/access_token.py
@@ -1,4 +1,3 @@
 class Token:
     def get_access_token(self):
         return 'LdzF6aMvRFAAAAAAAAAAKraQYKh0Co-9FCXbnRzZoQCqwSsA3Z83fM-Zi3USSpPs'
-        #return 'FCDpb2HE8A4AAAAAAAAHBryb4Rch0IxFTI4BpztmYsCFDO5hhw9UYiGThd-vLt9i'
